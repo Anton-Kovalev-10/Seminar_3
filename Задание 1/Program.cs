9da6916ee39eea3aa5626dd243046f6fd5bc0b7c
Anton Kovalev <antoha91011@mail.ru>
2022-12-12 13:51:46 +0700

    Домашнее задание к семинару

---
+++ Задание 1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+14212 -> нет
+12821 -> дa
+23432 -> да
+*/
+
+Console.WriteLine("Введите пятизначное число");
+string numberStr=Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+int number1 = number / 10000;
+int number2 = number % 10000 / 1000;
+
+int number4 = number % 100 / 10;
+int number5 = number % 10;
+
+
+if (number1 == number5 && number2 == number4)
+    
+    {
+        Console.WriteLine("палиндром");
+    }
+
+else
+{
+    Console.WriteLine("Не палиндром");
+}
